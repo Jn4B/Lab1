6ae4f0b2dd27c2cf14da2ca79f8ee13b6a0a1ee6
Jana Barhouche <janabarhouche5@gmail.com>
2023-01-26 20:50:42 +0200

    initial info is provided

---
+++ Profile.java
@@ -0,0 +1,9 @@
+public class Profile {
+
+	public static void main (String [] args){
+
+	System.out.println("Name: Jana Barhouche ");
+	System.out.println("Major: Computer Engineering");
+	System.out.println("Year of Study: 2023");
+	}
+} 
